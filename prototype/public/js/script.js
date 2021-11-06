3283ab10883d43488cc1e0fda802ed2582283a1b
sharon-ku <71299609+sharon-ku@users.noreply.github.com>
2021-11-06 11:21:24 -0400

    Prot: reorganized files into "public" folder; added express.js to node application to create static files (aka show my script.js!)

---
+++ prototype/public/js/script.js
@@ -0,0 +1,18 @@
+/**
+Greenhouse Hub Prototype
+Sharon Ku & Leanne Suen Fa
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(51);
+  ellipse(mouseX, mouseY, 60);
+}
--- prototype/server.js
+++ prototype/server.js
@@ -6,9 +6,14 @@ Implementing our http server
 let express = require("express");
 
 // Create an instance of the Express object to access its methods and properties
-let app = express();
+const app = express();
 
-// Listen on port 3000 (view on browser using "localhost:3000")
-let server = app.listen(3000);
+const portNumber = 2;
+
+// Listen on port 2 (view on browser using "localhost:2")
+let server = app.listen(portNumber);
+
+// When user goes on website, they see what's in the "public" directory
+app.use(express.static("public"));
 
 console.log("My socket server is running");
