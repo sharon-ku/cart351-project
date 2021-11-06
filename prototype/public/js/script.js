eaacadc3a233c8de5af36a83a203a41832b64482
sharon-ku <71299609+sharon-ku@users.noreply.github.com>
2021-11-06 11:37:53 -0400

    Prot: created all simulation states in script.js

--- prototype/public/js/script.js
+++ prototype/public/js/script.js
@@ -8,11 +8,47 @@ author, and this description to match your project!
 
 "use strict";
 
+// All possible states:
+// welcome, new-user-dialog, choose-pod, choose-podmate, choose-seed, pod-navigation, plant-view
+let state = `welcome`;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  // For testing only (Leanne could change this)
   background(51);
   ellipse(mouseX, mouseY, 60);
+
+  // States setup:
+  if (state === `welcome`) {
+    welcome();
+  } else if (state === `new-user-dialog`) {
+    newUserDialog();
+  } else if (state === `choose-pod`) {
+    choosePod();
+  } else if (state === `choose-podmate`) {
+    choosePodmate();
+  } else if (state === `choose-seed`) {
+    chooseSeed();
+  } else if (state === `pod-navigation`) {
+    podNavigation();
+  } else if (state === `plant-view`) {
+    plantView();
+  }
 }
+
+function welcome() {}
+
+function newUserDialog() {}
+
+function choosePod() {}
+
+function choosePodmate() {}
+
+function chooseSeed() {}
+
+function podNavigation() {}
+
+function plantView() {}
