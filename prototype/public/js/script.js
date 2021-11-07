65403b22060c40da1cd639134380e429c28e3be4
lksuenfa <70963322+lksuenfa@users.noreply.github.com>
2021-11-07 07:39:10 -0500

    Prot: input data

--- prototype/public/js/script.js
+++ prototype/public/js/script.js
@@ -7,9 +7,9 @@ author, and this description to match your project!
 */
 
 "use strict";
-let text = {
+let textSettings = {
   bodyFont: "Quicksand",
-  HeaderFont: "Gluten",
+  headerFont: "Gluten",
   size: 14,
 };
 // colours
@@ -25,17 +25,39 @@ let teal = {
   b: 123,
 };
 
+let input;
+let button;
+let buttonImg;
 // All possible states:
 // welcome, new-user-dialog, choose-pod, choose-podmate, choose-seed, pod-navigation, plant-view
 let state = `welcome`;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  buttonImg = loadImage("assets/images/arrow.png");
+
+  if (state === `welcome`) {
+    let inputX = windowWidth * (1 / 3);
+    let inputY = windowHeight / 2;
+    let inputSize = 400;
+
+    input = createInput("username");
+    input.position(inputX, inputY);
+    input.size(inputSize);
+
+    // let submitIcon = image(buttonImg, inputX + inputSize * 0.75, inputY, 50, 50);
+
+    button = createImg("assets/images/arrow.png");
+    button.size(100, 100);
+    button.position(inputX + inputSize, inputY);
+    button.mousePressed(changeState);
+  }
 }
 
 function draw() {
   // For testing only (Leanne could change this)
   background(teal.r, teal.g, teal.b);
+
   ellipse(mouseX, mouseY, 60);
 
   // States setup:
@@ -59,16 +81,32 @@ function draw() {
 function welcome() {
   push();
   fill(aqua.r, aqua.g, aqua.b);
-  textSize(text.size * 4);
-  textAlign(CENTER, CENTER);
-  textFont(text.bodyFont);
-  text("Welcome to Greenhouse hub", windowWidth / 2, windowHeight / 2);
+  textSize(textSettings.size * 3);
+  textAlign(LEFT, CENTER);
+  textFont(textSettings.headerFont);
+  text("Welcome to Greenhouse hub", 50, 100);
   pop();
 }
 
-function newUserDialog() {}
+function newUserDialog() {
+  push();
+  fill(aqua.r, aqua.g, aqua.b);
+  textSize(textSettings.size * 4);
+  textAlign(CENTER, CENTER);
+  textFont(textSettings.headerFont);
+  text("new User dialog", windowWidth / 2, windowHeight / 2);
+  pop();
+}
 
-function choosePod() {}
+function choosePod() {
+  push();
+  fill(aqua.r, aqua.g, aqua.b);
+  textSize(textSettings.size * 4);
+  textAlign(CENTER, CENTER);
+  textFont(textSettings.headerFont);
+  text("choosePod", windowWidth / 2, windowHeight / 2);
+  pop();
+}
 
 function choosePodmate() {}
 
@@ -77,3 +115,10 @@ function chooseSeed() {}
 function podNavigation() {}
 
 function plantView() {}
+
+// function mousePressed() {}
+function changeState() {
+  if ((state = `welcome`)) {
+    state = `new-user-dialog`;
+  }
+}
---
+++ prototype/public/read.md
@@ -0,0 +1,2 @@
+How to add image as button
+https://gist.github.com/lizzybrooks/54045563e4e8321718cc40297db999f9
