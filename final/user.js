3f69df444737cf2f112d3e959359be5224db7b99
sharon-ku <71299609+sharon-ku@users.noreply.github.com>
2021-12-01 10:20:52 -0500

    F: deleted 1 Greenhouse.js file

--- final/user.js
+++ final/user.js
@@ -1,32 +1,32 @@
 //NEW
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 //The ids of the fruits are stored within the user document as an array of Mongo ids.
 //The definition is as follows:
 
 const userSchema = new mongoose.Schema({
   user: String,
-  name: String,
+  // name: String,
   passwordHash: String,
   fruits: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Fruit'
-    }
+      ref: "Fruit",
+    },
   ],
-})
+});
 
-userSchema.set('toJSON', {
+userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
     // the passwordHash should not be revealed
-    delete returnedObject.passwordHash
-  }
-})
+    delete returnedObject.passwordHash;
+  },
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
